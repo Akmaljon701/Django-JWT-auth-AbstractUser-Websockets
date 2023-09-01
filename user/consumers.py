633f4fe65779fbb23f8c88a1a51dc7298a4812e6
Akmaljon <akmalkhanever@gmail.com>
2023-09-02 00:17:31 +0500

    added websocket-auth

--- user/consumers.py
+++ user/consumers.py
@@ -1,15 +1,28 @@
+from rest_framework_simplejwt.tokens import AccessToken
+
 from .serializers import *
-
-
 from asgiref.sync import sync_to_async
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Ism
 
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 
 class IsmlarConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
+        token = self.scope.get("query_string").decode("utf-8")
+        if token.startswith("token="):
+            token = token.replace("token=", "")    # tokenni o'zini ovolamiz
+            # Tokenni tekshirib ko'rish
+            try:
+                access_token = AccessToken(token)
+                user_id = access_token['user_id']
+                self.scope["user_id"] = user_id      # user id olindi
+            except Exception as e:
+                await self.close()      # Token sinab ko'rishda xatolik, ulanishni rad etish
+
         await self.accept()
         await self.channel_layer.group_add("ism_group", self.channel_name)
         await self.send_initial_ism_list()    # 1-martta Connect bo'lganda hamma malumotlar chiqishi
@@ -18,14 +31,24 @@ class IsmlarConsumer(AsyncWebsocketConsumer):
         await self.channel_layer.group_discard("ism_group", self.channel_name)   # Connectlarni tugatish uchun
 
     async def send_initial_ism_list(self):
-        ism_list = await self.get_ism_list()  # funksiya bo'yicha malumotlarni yuborish
-        await self.send(text_data=json.dumps(ism_list))
+        user_id = self.scope['user_id']
+        user = await self.get_user_by_id(user_id)
+        if user.role == "superadmin":   # ismlarni korish uchun faqat superadmin role ga ruhsat berish
+            ism_list = await self.get_ism_list()  # funksiya bo'yicha malumotlarni yuborish
+            await self.send(text_data=json.dumps(ism_list))
+        else:
+            await self.send(text_data=json.dumps({"message": "Sizga ismlar ro'yxati ko'rish huquqi yo'q."}))
 
     async def add_new_ism(self, event):       # yangi malumot kelsa event qabul qiladi va shu yerdan yangi malumotlar yuboriladi
         await self.send_initial_ism_list()
 
+    # asinxron qilib olish async def ichida sync ishlatib bomidi shunichun
     @sync_to_async
     def get_ism_list(self):        # ko'p joyda fodalanish uchun umumiy finksiya
         ism_objects = Ism.objects.all().order_by('-id')
         serializer = IsmSerializer(ism_objects, many=True)
         return serializer.data
+
+    @sync_to_async
+    def get_user_by_id(self, user_id):      # user malumotlarini olib olish
+        return CustomUser.objects.filter(id=user_id).first()
